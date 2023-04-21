9708ee752c61787291d4ad047f1b493d9528f737
Juan Londono <juanele211@.gmail.com>
2023-04-20 20:25:07 -0500

    Comments Section was added on product_info.php

--- index_admin.php
+++ index_admin.php
@@ -181,6 +181,21 @@
                             <h4><?php echo $row['category_name']?></h4>
                             <p><?php echo $description?></p>                                            
                         </div>
+                        <div class="comments">
+                            <p class="small text-muted">Comments: 
+                                <?php
+                                    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+                                    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                    $stmt = $pdo->prepare("SELECT COUNT(*) as cant FROM ts_comment WHERE product_id = :id");
+                                    $stmt->bindParam(':id', $id);
+                                    $stmt->execute();
+                                    $result = $stmt->fetch();
+                                    $cant_comments = $result['cant'];
+                                    echo $cant_comments;
+                                ?>                        
+                            </p>   
+                        </div>
+
                         <div class="price">
                             <p>$<?php echo number_format($price, 2) ?></p>                            
                         </div>
--- product_info.php
+++ product_info.php
@@ -1,5 +1,28 @@
 <?php
-    include ("_connect.php");           
+    include ("_connect.php");
+
+    if (isset($_POST['submit'])) {
+        // Get the values of the form
+        $product_id = $_GET['id'];
+        $comment_title = $_POST['comment_title'];
+        $user_comment = $_POST['user_comment'];        
+    
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+        // Prepare the SQL query to insert the comment
+        $stmt = $pdo->prepare("INSERT INTO ts_comment (product_id, comment_title, user_comment) VALUES (:product_id, :comment_title, :user_comment)");
+        $stmt->bindParam(':product_id', $product_id);
+        $stmt->bindParam(':comment_title', $comment_title);
+        $stmt->bindParam(':user_comment', $user_comment);
+    
+        // Execute the SQL query
+        $stmt->execute();
+    
+        // Redirect the user to the home page
+        header('Location: #');
+        exit;
+    }
 ?>
 
 <!DOCTYPE html>
@@ -14,7 +37,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
     <link rel="stylesheet" href="style.css">    
-<title>Tech Shop</title>
+<title>Product Information</title>
 </head>
 <body>
     <header>
@@ -42,7 +65,7 @@
 
         <div class="product_container">
             <div class="product_image"> 
-                <img src="<?php echo $_GET['image_url']; ?>" width="500px">
+                <img src="<?php echo $_GET['image_url']; ?>" width="400px">
             </div>            
             <div class="product_info">
                 <h4><?php echo $_GET['manufacturer']; ?> - <?php echo $_GET['reference']; ?></h4><br>
@@ -51,8 +74,50 @@
                 <p><strong>Price:</strong><p class="price-info"><?php echo '$' . number_format($_GET['price'], 2); ?></p></p>
             </div>
         </div>
+        <section class="row">
+            <div class="col">
+                <h4>Add Comment</h4><br>
+                <form method="POST">                    
+                    <input type="text" name="comment_title" placeholder="Publish Title"> <br><br>
+                    <textarea name="user_comment" placeholder="Add the comments here" class="form-control" rows="3" cols="30" required></textarea><br>
+                    <button type="submit" class="btn btn-success" name="submit">SAVE</button>
+                    <a href="index.php" class="btn btn-danger">CANCEL</a>
+                </form><br>                
+            </div>
+            <div class="col">
+                <h4 class="comments">Comments</h4><br>
+                    <?php           
+                    
+                        // Get the product ID
+                        $product_id = $_GET['id'];
+
+                        // Connect to the database
+                        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
+                        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                        
+                        // Prepare the SQL query to retrieve comments for this product
+                        $stmt = $pdo->prepare("SELECT comment_title, user_comment, created_at 
+                                            FROM ts_comment 
+                                            WHERE product_id = :product_id 
+                                            ORDER BY created_at DESC");
+                        $stmt->bindParam(':product_id', $product_id);
+                        $stmt->execute();
+
+                        // Print each comment
+                        while ($row = $stmt->fetch()) {
+                            $comment_date = $row['created_at'];
+                            echo '<div>';
+                    ?>
+                        <p class="title_comment"><?php echo $row['comment_title']?></p>
+                        <p class="small text-muted">Commented on: <?php  echo date("Y-m-d", strtotime($row  ['created_at']))?></p>
+                        <p class="content_comment"><?php echo $row['user_comment']?></p>
+                    <?php
+                        echo '</div><hr>';
+                    }
+                    ?>
+            </div>
+        </section>        
     </div>
-    
     
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
